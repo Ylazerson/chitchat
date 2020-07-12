44c3b7e9232fcbebea6b44fa067ac3349ba45d0d
j.lazerson <jake.lazerson@valuecentric.com>
2020-07-12 07:24:46 -0400

    for junk route

--- route_junk.go
+++ route_junk.go
@@ -11,7 +11,7 @@ func junk(writer http.ResponseWriter, request *http.Request) {
 	gizmos, err := data.Gizmos()
 
 	if err != nil {
-		error_message(writer, request, "Cannot get threads")
+		error_message(writer, request, "Cannot get gizmos")
 	} else {
 
 		generateHTML(writer, gizmos, "layout", "public.navbar", "junk")
